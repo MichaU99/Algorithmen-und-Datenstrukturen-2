c62baa8704aa1068687a78695cc99236a46c3888
MichaU99 <50555533+MichaU99@users.noreply.github.com>
2020-10-24 19:01:23 +0200

    dump Methode für UNSERE Testarbeit implementiert, benötigt weitere ueberarbeitung fuer abgabe

--- Uebung1_Streuwerttabellen/Algo2_Uebung1_Streuwerttabellen/src/HashTableChaining.java
+++ Uebung1_Streuwerttabellen/Algo2_Uebung1_Streuwerttabellen/src/HashTableChaining.java
@@ -25,30 +25,29 @@ public class HashTableChaining implements HashTable {
         Object tmp;
 
         index=key.hashCode(); //Unbekannt ob es stimmt, dummy implementation für weiteren code
+        if (key==null && val==null) return false;
         if(hashtable[index][0] instanceof Object){ //sollte testen ob bereits ein Objekt ins Array an der Stelle index eingefügt wurde
             tmp=hashtable[index][0];
             hashtable[index][0]=val;
-            for (tiefe=1;tiefe<=verkettung_len;tiefe++){ //Checkt ob im aktuellen Array genug Platz in der zweiten Dimension für einen weiteren Wert ist
-                if(!(hashtable[index][tiefe] instanceof Object)){
-                    hashtable[index][tiefe]=tmp;
-                    verlaengern=false;
+             for (tiefe = 1; tiefe <= verkettung_len-1; tiefe++) { //Checkt ob im aktuellen Array genug Platz in der zweiten Dimension für einen weiteren Wert ist
+                if (!(hashtable[index][tiefe] instanceof Object)) {
+                    hashtable[index][tiefe] = tmp;
+                    verlaengern = false;
                     break;
                 }
             }
             if (verlaengern){
                 array_tiefe_vergroessen(hashtable);
-                hashtable[index][verkettung_len]=tmp;
+                hashtable[index][verkettung_len-1]=tmp;
             }
             verlaengern=true; //Reset
-
+            return true;
         }
         else{
             hashtable[index][0]=val;
             return true;
         }
-            //Zum Chainen muss an einer Stelle x ein Objekt einen Verweis auf eine Stelle y haben können die nicht im Bereich des normalen Arrays liegt, mögliche Implementation mithilfe von sich selbst vergrößernden mehrdimensionalen Arrays oder Zeigern?
-        if (key==null && val==null) return false;
-        return false;
+
     }
 
     @Override
@@ -61,21 +60,36 @@ public class HashTableChaining implements HashTable {
         return false;
     }
 
-    @Override
+    @Override //Implementierung stimmt nicht!!!! Muss vor abgabe noch geändert werden, wie müssen die gechainten Elemente nummeriert werden???
     public void dump() {
+        int[] tiefenfrage=new int[100];
 
+        int i,j; //lauf
+        for (i=0;i<=N-1;i++){
+            for(j=0;j<=verkettung_len-1;j++){
+                if(hashtable[i][j]==null) break;
+                else System.out.println("Stelle im Table key:"+i+" chaindeapth:"+j+" val:"+hashtable[i][j]);
+            }
+            tiefenfrage[i]=j;
+        }
+        int test=0;
+        for(i=0;i<=N-1;i++){
+            System.out.print(tiefenfrage[i]+" ");
+            test=test+tiefenfrage[i];
+        }
+
+        System.out.println();
+        System.out.println("du bists "+test);
     }
     public void array_tiefe_vergroessen(Object[][] array){ //Vergroessert HashTable in zweiter Dimension (verkettung_len) um 1 und kopiert alle Elemente aus dem uebergebenen Array, speichert das Ergebnis in HashTable
         Object[][] tmp_arr=new Object[N][++verkettung_len];
         int i,a; //Laufvariablen
-
-        for(i=0;i<=(array.length-1);i++){
-            for (a=0;a<=1;a++){
+        for(i=0;i<=N-1;i++){
+            for (a=0;a<=verkettung_len-2;a++){
                 tmp_arr[i][a]=array[i][a];
             }
         }
-        hashtable=tmp_arr;
+        hashtable=tmp_arr;    }
 
-    }
 
 }
--- Uebung1_Streuwerttabellen/Algo2_Uebung1_Streuwerttabellen/src/Simple_Test.java
+++ Uebung1_Streuwerttabellen/Algo2_Uebung1_Streuwerttabellen/src/Simple_Test.java
@@ -1,6 +1,10 @@
 import java.lang.Math;
+import java.util.Random;
+
 public class Simple_Test {
     public static void main(String[] args){
+        Random random= new Random();
+        int i;
     /*MultiplicationMethod h = new MultiplicationMethod(10, (int) (((Math.sqrt(5)-1)/2)*Math.pow(2,32)));
         System.out.println((h.compute(500))) ;
         DivisionMethod d=new DivisionMethod((int) (((Math.sqrt(5)-1)/2)*Math.pow(2,32)));
@@ -16,7 +20,9 @@ public class Simple_Test {
                 return 0;
             }
         });
-        System.out.println(hashtable.put(5,10));
-
+        for(i=0;i<=10000;i++){
+            hashtable.put(random.nextInt(100),random.nextInt(101));
+        }
+        hashtable.dump();
     }
 }
