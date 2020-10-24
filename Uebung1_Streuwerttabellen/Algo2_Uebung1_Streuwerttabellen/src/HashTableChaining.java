9b8938e8c2906e7df60bcc723d2aca6abb10887a
MichaU99 <50555533+MichaU99@users.noreply.github.com>
2020-10-24 13:06:00 +0200

    Kommentare für HashTable Implementierung

--- Uebung1_Streuwerttabellen/Algo2_Uebung1_Streuwerttabellen/src/HashTableChaining.java
+++ Uebung1_Streuwerttabellen/Algo2_Uebung1_Streuwerttabellen/src/HashTableChaining.java
@@ -1,9 +1,13 @@
+import java.lang.reflect.Array;
+
 // Implementierung von Streuwerttabellen mit Verkettung.
-import java.util.ArrayList;
 public class HashTableChaining implements HashTable {
     // Streuwerttabelle mit Streuwertfunktion f.
 
-    int N=5; //groesse
+    //Java besitzt unter java.util.HashMap selbst eine Implementation einer Hashmap, können/ sollen wir die benutzen oder Eigenimplementation?
+
+    int N=100; //groesse
+    int[] hashtable=new int[N];
 
     public HashTableChaining(HashFunction f) {
 
@@ -11,6 +15,12 @@ public class HashTableChaining implements HashTable {
 
     @Override
     public boolean put(Object key, Object val) {
+        int index;
+
+        index=key.hashCode(); //Unbekannt ob es stimmt, dummy implementation für weiteren code
+        if(hashtable[index]) //Man benötigt Information ob im Array an Stelle x bereits ein Objekt gespeichert ist, mögliche Implementation über zweidimensionales Array mit Info in der zweiten Dimension
+            //Zum Chainen muss an einer Stelle x ein Objekt einen Verweis auf eine Stelle y haben können die nicht im Bereich des normalen Arrays liegt, mögliche Implementation mithilfe von sich selbst vergrößernden mehrdimensionalen Arrays oder Zeigern?
+        if (key==null && val==null) return false;
         return false;
     }
 
--- Uebung1_Streuwerttabellen/Algo2_Uebung1_Streuwerttabellen/src/Simple_Test.java
+++ Uebung1_Streuwerttabellen/Algo2_Uebung1_Streuwerttabellen/src/Simple_Test.java
@@ -3,5 +3,7 @@ public class Simple_Test {
     public static void main(String[] args){
     MultiplicationMethod h = new MultiplicationMethod(10, (int) (((Math.sqrt(5)-1)/2)*Math.pow(2,32)));
         System.out.println((h.compute(500))) ;
+        DivisionMethod d=new DivisionMethod((int) (((Math.sqrt(5)-1)/2)*Math.pow(2,32)));
+        System.out.println(d.compute(500));
     }
 }
