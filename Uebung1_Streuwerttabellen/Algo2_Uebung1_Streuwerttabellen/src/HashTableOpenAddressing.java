4caf69e560f338a50d1f28af0df4488c237ed743
MichaU99 <50555533+MichaU99@users.noreply.github.com>
2020-10-30 13:32:19 +0100

    Testweise Implementierung OpenAdressing, neue Klasse Speicher hinzugefuegt

--- Uebung1_Streuwerttabellen/Algo2_Uebung1_Streuwerttabellen/src/HashTableOpenAddressing.java
+++ Uebung1_Streuwerttabellen/Algo2_Uebung1_Streuwerttabellen/src/HashTableOpenAddressing.java
@@ -1,11 +1,30 @@
 // Implementierung von Streuwerttabellen mit offener Adressierung.
 public class HashTableOpenAddressing implements HashTable{
     // Streuwerttabelle mit Sondierungsfunktion s.
+    HashSequence HSequence;
+    int N; //Groesse der Tabelle
+    Speicher[] hashtable;
+
     public HashTableOpenAddressing (HashSequence s) {
+        this.HSequence=s;
+        this.N=HSequence.size();
+        hashtable=new Speicher[N];
     }
 
     @Override
     public boolean put(Object key, Object val) {
+        int index;
+
+        if(key==null && val==null) return false; //Fehlerhafte Eingabe abfangen
+
+        index=HSequence.first(key); //Ersten Index berechnen & testen
+        if(hashtable[index]==null) hashtable[index]=new Speicher(key, val);
+        else{ //Sucht alternativen Speicherpunkt
+            while (true){
+
+            }
+        }
+
         return false;
     }
 
