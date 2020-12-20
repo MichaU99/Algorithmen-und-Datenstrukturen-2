dc0b5e75d89e484078352a0628bee2b7a449c076
MichaU99 <50555533+MichaU99@users.noreply.github.com>
2020-12-20 20:47:19 +0100

    added the beginnings of SCCImpl

--- Uebung3_Graphenalgorithmen/src/SCCImpl.java
+++ Uebung3_Graphenalgorithmen/src/SCCImpl.java
@@ -1,6 +1,14 @@
 public class SCCImpl implements SCC{
+    int[] graph1=null;
+    int[] graph2=null;
+
     @Override
     public void compute(Graph g) {
+        DFS tiefensucheG=new DFSImpl();
+        DFS tiefensucheGtlive=new DFSImpl();
+        tiefensucheG.search(g);
+        graph1=new int[g.size()];
+        tiefensucheGtlive.search(g.transpose(),tiefensucheG); //Wie schaffe ich hier eine absteigende Reihenfolge in der äußeren Schleife?
 
     }
 
