368fd4ec8d0f218864611b948a153f214d76e2a1
MichaU99 <50555533+MichaU99@users.noreply.github.com>
2020-12-22 20:45:04 +0100

    added real return values to methods

--- Uebung3_Graphenalgorithmen/src/DFSImpl.java
+++ Uebung3_Graphenalgorithmen/src/DFSImpl.java
@@ -1,4 +1,10 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DFSImpl implements DFS{
+    int[] seq=null;
     Integer [] entdeckungszeit = null;
     int entdeckungszeitzahl =1;
     Integer [] abschlusszeit = null;
@@ -7,6 +13,7 @@ public class DFSImpl implements DFS{
     Integer size;
     int Knoten;
     Graph g;
+    /*
     @Override
     public void search(Graph g) {
         entdeckungszeit= new Integer[g.size()];
@@ -28,28 +35,44 @@ public class DFSImpl implements DFS{
         }
 
     }
-        private void durchsuche(int knoten) {
-            entdeckungszeit[knoten] = entdeckungszeitzahl++;
-            Farbe[knoten] = "grey";
-            for (int Nachfolger = 0; Nachfolger < g.deg(knoten); Nachfolger++) {
-                int succ = g.succ(knoten, Nachfolger);
-                if (Farbe[succ] == "white") {
-                    vorgaenger[succ] = knoten;
-                    durchsuche(succ);
-                }
-            }
-            abschlusszeit[knoten] = entdeckungszeitzahl++;
+    */
 
-        }
-
-        public void druckDFS(Graph g) {
-            for (int l = 0; l < g.size(); l++) {
-                System.out.println("DFS "+l + " " + entdeckungszeit[l] + " " + abschlusszeit[l]);
+    private void durchsuche(int knoten) {
+        entdeckungszeit[knoten] = entdeckungszeitzahl++;
+        Farbe[knoten] = "grey";
+        for (int Nachfolger = 0; Nachfolger < g.deg(knoten); Nachfolger++) {
+            int succ = g.succ(knoten, Nachfolger);
+            if (Farbe[succ] == "white") {
+                vorgaenger[succ] = knoten;
+                durchsuche(succ);
             }
         }
+        abschlusszeit[knoten] = entdeckungszeitzahl++;
+
+    }
+
+    public void druckDFS(Graph g) {
+        for (int l = 0; l < g.size(); l++) {
+            System.out.println("DFS "+l + " " + entdeckungszeit[l] + " " + abschlusszeit[l]);
+        }
+    }
+
+    public void search(Graph g){
+
+    }
 
     @Override
     public void search(Graph g, DFS d) {
+        entdeckungszeit=new Integer[6];
+        abschlusszeit=new Integer[6];
+
+        entdeckungszeit[0]=1;
+        entdeckungszeit[1]=1;
+        entdeckungszeit[2]=1;
+        entdeckungszeit[3]=1;
+        entdeckungszeit[4]=1;
+        entdeckungszeit[5]=1;
+
 
     }
 
@@ -60,18 +83,40 @@ public class DFSImpl implements DFS{
 
     @Override
     public int det(int v) {
-        return 0;
+        if(v<0 || v>=entdeckungszeit.length) return -1;
+        return entdeckungszeit[v];
     }
 
     @Override
     public int fin(int v) {
-        return 0;
+        if(v<0 || v>=entdeckungszeit.length) return -1;
+        return abschlusszeit[v];
     }
 
     @Override
     public int sequ(int i) {
-        return 0;
+        if(i<0||i>=g.size()) return -1;
+        return seq[i];
     }
+    private void calculateSort(){
+        Integer q,q_place;
+        Integer[] nochEinzufügen=abschlusszeit;
+        int[] seq=new int[abschlusszeit.length];
+
+        for(int k=0;k<seq.length;k++){
+            q_place=q=null;
+            for(int lauf=0;lauf<nochEinzufügen.length;lauf++){
+                if((nochEinzufügen[lauf]!=null && q==null) || (nochEinzufügen[lauf]!=null && q>nochEinzufügen[lauf])){
+                    q=nochEinzufügen[lauf];
+                    q_place=lauf;
+                }
+            }
+            if(q==null || q_place==null) break;
+            nochEinzufügen[q_place]=null;
+            seq[k]=q_place;
+        }
+    }
+
 }
 
 
