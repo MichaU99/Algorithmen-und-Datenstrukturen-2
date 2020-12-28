ad5993ac87c8b7331bb42fef35af64756157c0e4
MichaU99 <50555533+MichaU99@users.noreply.github.com>
2020-12-28 19:32:19 +0100

    Changed some stuff

--- Uebung3_Graphenalgorithmen/src/DefaultTestG.java
+++ Uebung3_Graphenalgorithmen/src/DefaultTestG.java
@@ -13,7 +13,7 @@ public class DefaultTestG {
             System.out.println("Test beginnt");
         }
 
-        String testsToDo = "Tiefensuche";//Put in the tests you want to perform (test1 test2 test3 usw)
+        String testsToDo = "GraphImpl";//Put in the tests you want to perform (test1 test2 test3 usw)
         for (String list : testsToDo.split(" ")) {
 
             switch (list) {
@@ -34,23 +34,20 @@ public class DefaultTestG {
                     for (int i = 0; i < ArrayList.length; i++) {
                         assert (graph.deg(i) == ArrayList[i]) : "Der Knoten " + i + " sollte " + ArrayList[i] + " Nachfolger haben, ist aber " + graph.deg(i);
                     }
-                    for (int i = 0; i < ArrayList.length; i++) {
-                        // TODO: 28.12.2020 Testen
-                        kakau:
+                    for (int i = 0; i < graph.size(); i++) {
                         for (int k = 0; k < graph.deg(i); k++) {
                             for (int j = 0; j < graph.deg(i) ;j++) {
-                                if (graph.succ(i, j) == Muster[i][k])
-                                    break kakau;
+                                if (graph.succ(i, j) == Muster[i][k]) break;
                             }
                             assert (true) : "Der Knoten " + i + " sollte " + ArrayList[i] + " als direkten Nachfolger haben, ist aber " + graph.succ(i, i);
                         }
                     }
                     int [][] Muster2 = new int[][] {
                             {1,3,4},
-                            {},
+                            {3},
                             {0,1},
-                            {4,6},
-                            {1,5},
+                            {4},
+                            {1,5,6},
                             {6},
                             {5,6},
                     };
@@ -60,7 +57,7 @@ public class DefaultTestG {
 
                 // Gerichteter gewichteter Graph
                 case "WeightedGraph":
-                    graph = new GraphImpl(new int [][] {
+                    WeightedGraph Wgraph = new WeightedGraphImpl(new int [][] {
                             {2},
                             {0,2,4},
                             {},
@@ -68,14 +65,13 @@ public class DefaultTestG {
                             {0,3},
                             {4,6},
                             {3,5,6},
-                    });
+                    },new double[][]{});
                     // Hier ist noch Baustelle
-                    WeightedGraph graphWeighted = new WeightedGraph();
                     ArrayList = new int [] {};
                     for (int i = 0; i < ArrayList.length; i++) {
-                        assert (graph.weight(i)) : " ";
-                    //WeightedGraph.weight(1, 1);
+                        assert (Wgraph.weight(i, 0) == -2) : " "; // TODO: 28.12.2020
 
+                    }
                     break;
 
                 // Breitensuche
@@ -171,6 +167,8 @@ public class DefaultTestG {
         System.out.println("Alle Tests wurden bestanden");
          */
 
+                default:
+                    System.out.println("Fehlerhafte Eingabe bei der Testfallwahl");
                     }
         }
     }
