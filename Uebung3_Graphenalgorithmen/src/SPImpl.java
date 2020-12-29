5f0e8b953aa7d989c84466152a9eeab1051fd405
Veronika <veronika_scheller@web.de>
2020-12-29 23:37:23 +0100

    spi Bellman fertig

--- Uebung3_Graphenalgorithmen/src/SPImpl.java
+++ Uebung3_Graphenalgorithmen/src/SPImpl.java
@@ -7,43 +7,52 @@ public class SPImpl implements SP{
     public boolean bellmanFord(WeightedGraph g, int s) {
         size = g.size();
         strartknoten = s;
-        AbstandZuStartknoten= new double[size];
+        AbstandZuStartknoten = new double[size];
         vorgaenger = new Integer[size];
-        for(int i=0; i<size;i++){
-            AbstandZuStartknoten[i]=INF;
-            vorgaenger[i]=NIL;
+        for (int i = 0; i < size; i++) {
+            AbstandZuStartknoten[i] = INF;
+            vorgaenger[i] = NIL;
         }
-        AbstandZuStartknoten[s]=0;
-        for(int i=0; i<size-1;i++){
+        AbstandZuStartknoten[s] = 0;
 
-            for(int Knoten=0;Knoten < size; Knoten++){
-                if (Knoten == strartknoten){ // wie ist der vergleich hier? oder equals?
-                    for(int nachfolger=0; nachfolger<g.deg(s); nachfolger++){
-                        int NachfolgerKnoten=g.succ(s,nachfolger);
-                        AbstandZuStartknoten[NachfolgerKnoten]= g.weight(s,NachfolgerKnoten);
-                        vorgaenger[NachfolgerKnoten]=s;
+        for (int i = 0; i < size - 1; i++) {
+
+            for (int Knoten = 0; Knoten < size; Knoten++) {
+                if (Knoten == strartknoten) { // wie ist der vergleich hier? oder equals?
+                    for (int nachfolger = 0; nachfolger < g.deg(s); nachfolger++) {
+                        int NachfolgerKnoten = g.succ(s, nachfolger);
+                        AbstandZuStartknoten[NachfolgerKnoten] = g.weight(s, NachfolgerKnoten);
+                        vorgaenger[NachfolgerKnoten] = s;
                     }
-                }
-                else{
-                    for(int nachfolger=0; nachfolger<g.deg(Knoten); nachfolger++){
-                        int NachFolgerKnoten=g.succ(Knoten,nachfolger);
-                        if( AbstandZuStartknoten [Knoten] == INF){
-
+                } else {
+                    for (int nachfolger = 0; nachfolger < g.deg(Knoten); nachfolger++) {
+                        int NachFolgerKnoten = g.succ(Knoten, nachfolger);
+                        if (AbstandZuStartknoten[Knoten] + g.weight(Knoten, NachFolgerKnoten) < AbstandZuStartknoten[NachFolgerKnoten]) {
+                            AbstandZuStartknoten[NachFolgerKnoten] = AbstandZuStartknoten[Knoten] + g.weight(Knoten, NachFolgerKnoten);
+                            vorgaenger[NachFolgerKnoten] = Knoten;
                         }
                     }
                 }
             }
         }
+        for (int Knoten = 0; Knoten < size; Knoten++) {
+            for (int nachfolger = 0; nachfolger < g.deg(Knoten); nachfolger++) {
+                int NachFolgerKnoten = g.succ(Knoten, nachfolger);
+                if (AbstandZuStartknoten[Knoten] + g.weight(Knoten, NachFolgerKnoten) < AbstandZuStartknoten[NachFolgerKnoten]) {
+                    return false;
+                }
+            }
+        }
 
-
-
-
-
-
-
-
-        return false;
+        return true;
     }
+
+
+
+
+
+
+
 
     @Override
     public void dijkstra(WeightedGraph g, int s) {
